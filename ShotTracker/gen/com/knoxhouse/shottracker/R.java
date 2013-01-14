c6ddbab248c82f1795bacaa751223e52198bbcbf
JasonHHouse <jh5975@gmail.com>
2013-01-13 18:55:24 -0800

    Added Profile Added Gun Viewer

--- ShotTracker/gen/com/knoxhouse/shottracker/R.java
+++ ShotTracker/gen/com/knoxhouse/shottracker/R.java
@@ -16,36 +16,63 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int ShooterProfile=0x7f070004;
-        public static final int imgShotTracker=0x7f070001;
-        public static final int listView1=0x7f070008;
-        public static final int menu_settings=0x7f070009;
-        public static final int txtGunVault=0x7f070003;
-        public static final int txtRangeDay=0x7f070005;
-        public static final int txtSearch=0x7f070007;
-        public static final int txtShot=0x7f070000;
-        public static final int txtTheVault=0x7f070006;
-        public static final int txtTracker=0x7f070002;
+        public static final int ShooterProfile=0x7f07000d;
+        public static final int imageView1=0x7f070000;
+        public static final int imgShotTracker=0x7f07000a;
+        public static final int lblAge=0x7f070011;
+        public static final int lblCaliber=0x7f070002;
+        public static final int lblGunName=0x7f070001;
+        public static final int lblManufactured=0x7f070005;
+        public static final int lblReloaded=0x7f070007;
+        public static final int lblYearsShooting=0x7f070013;
+        public static final int listManufactured=0x7f070006;
+        public static final int listReloaded=0x7f070008;
+        public static final int listView1=0x7f070017;
+        public static final int menu_settings=0x7f070018;
+        public static final int txtAge=0x7f070012;
+        public static final int txtAmmunition=0x7f070004;
+        public static final int txtCaliberValue=0x7f070003;
+        public static final int txtFirstName=0x7f07000f;
+        public static final int txtGunVault=0x7f07000c;
+        public static final int txtLastName=0x7f070010;
+        public static final int txtRangeDay=0x7f07000e;
+        public static final int txtSearch=0x7f070016;
+        public static final int txtShot=0x7f070009;
+        public static final int txtTheVault=0x7f070015;
+        public static final int txtTracker=0x7f07000b;
+        public static final int txtYearsShooting=0x7f070014;
     }
     public static final class layout {
-        public static final int activity_main_screen=0x7f030000;
-        public static final int activity_vault=0x7f030001;
+        public static final int activity_gun_viewer=0x7f030000;
+        public static final int activity_main_screen=0x7f030001;
+        public static final int activity_profile=0x7f030002;
+        public static final int activity_vault=0x7f030003;
     }
     public static final class menu {
-        public static final int activity_main_screen=0x7f060000;
-        public static final int activity_vault=0x7f060001;
+        public static final int activity_gun_viewer=0x7f060000;
+        public static final int activity_main_screen=0x7f060001;
+        public static final int activity_profile=0x7f060002;
+        public static final int activity_vault=0x7f060003;
     }
     public static final class string {
+        public static final int Age=0x7f04000c;
+        public static final int Ammunition=0x7f040010;
+        public static final int Caliber=0x7f04000f;
         public static final int GunVault=0x7f040004;
+        public static final int Manufactured=0x7f040011;
         public static final int RangeDay=0x7f040006;
+        public static final int Reloaded=0x7f040012;
         public static final int SHOT=0x7f040002;
         public static final int Search=0x7f04000a;
         public static final int ShooterProfile=0x7f040005;
         public static final int TRACKER=0x7f040003;
         public static final int TheVault=0x7f040009;
+        public static final int YearsShooting=0x7f04000d;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040007;
         public static final int menu_settings=0x7f040001;
+        public static final int title_activity_gun_viewer=0x7f04000e;
+        public static final int title_activity_profile=0x7f04000b;
         public static final int title_activity_vault=0x7f040008;
     }
     public static final class style {
---
+++ ShotTracker/src/com/knoxhouse/shottracker/GunViewerActivity.java
@@ -0,0 +1,22 @@
+package com.knoxhouse.shottracker;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GunViewerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gun_viewer);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_gun_viewer, menu);
+		return true;
+	}
+
+}
