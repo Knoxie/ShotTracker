80f224a84e73d74627cef394374d0eb8e5247459
JasonHHouse <jh5975@gmail.com>
2013-01-13 17:51:55 -0800

    Main Screen Update The Vault Screen Update

--- ShotTracker/gen/com/knoxhouse/shottracker/R.java
+++ ShotTracker/gen/com/knoxhouse/shottracker/R.java
@@ -11,22 +11,42 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int border=0x7f020000;
+        public static final int bottom_border=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070001;
-        public static final int textView1=0x7f070000;
+        public static final int ShooterProfile=0x7f070004;
+        public static final int imgShotTracker=0x7f070001;
+        public static final int listView1=0x7f070008;
+        public static final int menu_settings=0x7f070009;
+        public static final int txtGunVault=0x7f070003;
+        public static final int txtRangeDay=0x7f070005;
+        public static final int txtSearch=0x7f070007;
+        public static final int txtShot=0x7f070000;
+        public static final int txtTheVault=0x7f070006;
+        public static final int txtTracker=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main_screen=0x7f030000;
+        public static final int activity_vault=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main_screen=0x7f060000;
+        public static final int activity_vault=0x7f060001;
     }
     public static final class string {
+        public static final int GunVault=0x7f040004;
+        public static final int RangeDay=0x7f040006;
+        public static final int SHOT=0x7f040002;
+        public static final int Search=0x7f04000a;
+        public static final int ShooterProfile=0x7f040005;
+        public static final int TRACKER=0x7f040003;
+        public static final int TheVault=0x7f040009;
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int hello_world=0x7f040007;
+        public static final int menu_settings=0x7f040001;
+        public static final int title_activity_vault=0x7f040008;
     }
     public static final class style {
         /** 
